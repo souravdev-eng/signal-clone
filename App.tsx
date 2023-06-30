23088d6e0b1941927d6e7cc3fa46b7c5a8ebf62d
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-06-30 12:39:07 +0530

    Add Authantication

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from './navigations';
-import { Amplify } from 'aws-amplify'
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 import config from './src/aws-exports';
 
 Amplify.configure(config)
@@ -13,4 +14,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
