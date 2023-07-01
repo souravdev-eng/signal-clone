fdc9f2c8c8f26fb162ccbc7b204cfcb2273b47bf
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-07-01 10:13:07 +0530

    Fix amplify bug

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "signalclone": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
-    "signalclone": {
+    "SignalClone": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
